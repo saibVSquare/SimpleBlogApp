ff6587ee32c94d467f5f91c120274c2b890298be
umairch <umairchaudhary6161@gmail.com>
2024-01-10 22:42:58 +0500

    try useEffect

--- src/components/page/home.jsx
+++ src/components/page/home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FancyTextComponent from "../hoc/FancyTextComponent";
 import TextComponent from "../hoc/TextComponent";
 import Board from "../tictac";
@@ -9,6 +9,16 @@ const listItems = products.map((product) => (
 ));
 
 const HomePage = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Run UseEffect Hooks");
+  }, [count]);
+
+  const increaseCounter = () => {
+    setCount((e) => e + 1);
+  };
+
   return (
     <div className="container">
       <h1>Home Page</h1>
@@ -28,6 +38,9 @@ const HomePage = () => {
       <TextComponent text="Hello, this is Simple text!" />
       <FancyTextComponent text="Hello, this is fancy text!" />
       <FancyTextComponent text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio alias qui provident quam optio repudiandae at, assumenda perspiciatis perferendis distinctio reprehenderit odit sint autem delectus corporis magni cupiditate quisquam vel!" />
+      <p>
+        {count} <button onClick={increaseCounter}>+</button>
+      </p>
     </div>
   );
 };
